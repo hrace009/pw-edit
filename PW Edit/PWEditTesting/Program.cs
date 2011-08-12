8cf5c89d8ea5874a16a44f347f9610ea547ea9e6
Das7002 <das7002@gmail.com>
2011-08-12 01:26:16 +0000

    added test project to ease in debugging later on and added CreatureGroup reader

---
+++ PW Edit/PWEditTesting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PWEditLib.NPCGenData;
+
+namespace PWEditTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NPCGEN npcgen = new NPCGEN("C:\\Users\\David\\Desktop\\npcgen.data");
+        }
+    }
+}
