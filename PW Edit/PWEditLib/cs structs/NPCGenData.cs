4275edc07d6d29cf93b78708cde80120bf493964
Das7002 <das7002@gmail.com>
2011-08-30 00:59:33 +0000

    Finished NPCGEN loader and XmlSerializer for it (which is giant, anything that does fancy formatting will probably be slow as all hell if it doesn't crash), binary npcgen.data saving should be coming soon

--- PW Edit/PWEditLib/cs structs/NPCGenData.cs
+++ PW Edit/PWEditLib/cs structs/NPCGenData.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Xml.Serialization;
+using System.Diagnostics;
 
 namespace PWEditLib.NPCGenData
 {
@@ -87,7 +89,7 @@ namespace PWEditLib.NPCGenData
         /// <summary>
         /// Byte used instead of Unsigned Char
         /// </summary>
-        public Byte unknow5a;
+        public Byte unknown5a;
         /// <summary>
         /// Byte used instead of Unsigned Char
         /// </summary>
@@ -134,11 +136,10 @@ namespace PWEditLib.NPCGenData
     {
         public Int32 id;
         public Int32 gmID;
-        //public String name;
         /// <summary>
-        /// Declared as a byte array instead of a string due to fixed size in NPCGen
+        /// Set as byte array instead of String due to constant size limit
         /// </summary>
-        public Byte[] name = new Byte[128];
+        public Byte[] name;
         public Boolean autostart;
         public Int32 autostartDelay;
         public Int32 autostopDelay;
@@ -179,92 +180,235 @@ namespace PWEditLib.NPCGenData
         /// <param name="NPCGenFile">Path to the npcgen.data file</param>
         public NPCGEN(String NPCGenFile)
         {
-            load(NPCGenFile);
+            Load(NPCGenFile);
         }
         /// <summary>
         /// Load an npcgen.data file
         /// </summary>
         /// <param name="NPCGenFile">Path to the npcgen.data file</param>
         /// <returns>True on successful load</returns>
-        public Boolean load(String NPCGenFile)
+        public Boolean Load(String NPCGenFile)
         {
-            MemoryStream ms = new MemoryStream();
-            FileStream fs = new FileStream(NPCGenFile, FileMode.Open);
-            //Clone the FileStream into memory release and reset Position on MemoryStream
-            fs.CopyTo(ms); fs.Close(); ms.Position = 0;
-            BinaryReader br = new BinaryReader(ms);
-            creatureSets = new List<CreatureSet>();
-            resourceSets = new List<ResourceSet>();
-            dynamics = new List<DynamicObj>();
-            triggers = new List<Trigger>();
-            version = br.ReadInt32();
-            creatureSetsCount = br.ReadInt32();
-            resourceSetsCount = br.ReadInt32();
-            dynamicsCount = br.ReadInt32();
-            triggersCount = br.ReadInt32();
-            Int32 i;
-            Int32 x;
-            CreatureSet creatureFillHolder;
-            CreatureGroup creatureGFillHolder;
-            for (i = 0; i < creatureSetsCount; i++)
+            try
             {
-                creatureFillHolder = new CreatureSet();
-                creatureFillHolder.spawnMode = br.ReadInt32();
-                creatureFillHolder.creatureGroupCount = br.ReadInt32();
-                creatureFillHolder.spawnX = br.ReadSingle();
-                creatureFillHolder.spawnY = br.ReadSingle();
-                creatureFillHolder.spawnZ = br.ReadSingle();
-                creatureFillHolder.rot1 = br.ReadSingle();
-                creatureFillHolder.rot2 = br.ReadSingle();
-                creatureFillHolder.rot3 = br.ReadSingle();
-                creatureFillHolder.spreadX = br.ReadSingle();
-                creatureFillHolder.spreadY = br.ReadSingle();
-                creatureFillHolder.spreadZ = br.ReadSingle();
-                creatureFillHolder.unknown1 = br.ReadInt32();
-                creatureFillHolder.unknown2 = br.ReadInt32();
-                creatureFillHolder.unknown3 = br.ReadBoolean();
-                creatureFillHolder.unknown4 = br.ReadBoolean();
-                creatureFillHolder.unknown5 = br.ReadBoolean();
-                creatureFillHolder.unknown6 = br.ReadInt32();
-                creatureFillHolder.trigger = br.ReadInt32();
-                creatureFillHolder.unknown7 = br.ReadInt32();
-                creatureFillHolder.unknown8 = br.ReadInt32();
-                creatureFillHolder.creatureGroups = new List<CreatureGroup>();
-                for (x = 0; x < creatureFillHolder.creatureGroupCount; x++)
+                MemoryStream ms = new MemoryStream();
+                FileStream fs = new FileStream(NPCGenFile, FileMode.Open);
+                //Clone the FileStream into memory release and reset Position on MemoryStream
+                fs.CopyTo(ms); fs.Close(); ms.Position = 0;
+                BinaryReader br = new BinaryReader(ms);
+                creatureSets = new List<CreatureSet>();
+                resourceSets = new List<ResourceSet>();
+                dynamics = new List<DynamicObj>();
+                triggers = new List<Trigger>();
+                version = br.ReadInt32();
+                creatureSetsCount = br.ReadInt32();
+                resourceSetsCount = br.ReadInt32();
+                dynamicsCount = br.ReadInt32();
+                triggersCount = br.ReadInt32();
+                Int32 i;
+                Int32 x;
+                CreatureSet creatureFillHolder;
+                CreatureGroup creatureGFillHolder;
+                for (i = 0; i < creatureSetsCount; i++)
                 {
-                    creatureGFillHolder = new CreatureGroup();
-                    creatureGFillHolder.id = br.ReadInt32();
-                    creatureGFillHolder.amount = br.ReadInt32();
-                    creatureGFillHolder.respawn = br.ReadInt32();
-                    creatureGFillHolder.diedTimes = br.ReadInt32();
-                    creatureGFillHolder.agressive = br.ReadInt32();
-                    creatureGFillHolder.offsetWater = br.ReadInt32();
-                    creatureGFillHolder.offsetTrn = br.ReadInt32();
-                    creatureGFillHolder.faction = br.ReadInt32();
-                    creatureGFillHolder.facHelper = br.ReadInt32();
-                    creatureGFillHolder.facAccept = br.ReadInt32();
-                    creatureGFillHolder.needHelp = br.ReadBoolean();
-                    creatureGFillHolder.defFaction = br.ReadBoolean();
-                    creatureGFillHolder.defFacHelper = br.ReadBoolean();
-                    creatureGFillHolder.defFacAccept = br.ReadBoolean();
-                    creatureGFillHolder.pathID = br.ReadInt32();
-                    creatureGFillHolder.loopType = br.ReadInt32();
-                    creatureGFillHolder.speedFlag = br.ReadInt32();
-                    creatureGFillHolder.deadTime = br.ReadInt32();
-                    creatureFillHolder.creatureGroups.Add(creatureGFillHolder);
+                    creatureFillHolder = new CreatureSet();
+                    creatureFillHolder.spawnMode = br.ReadInt32();
+                    creatureFillHolder.creatureGroupCount = br.ReadInt32();
+                    creatureFillHolder.spawnX = br.ReadSingle();
+                    creatureFillHolder.spawnY = br.ReadSingle();
+                    creatureFillHolder.spawnZ = br.ReadSingle();
+                    creatureFillHolder.rot1 = br.ReadSingle();
+                    creatureFillHolder.rot2 = br.ReadSingle();
+                    creatureFillHolder.rot3 = br.ReadSingle();
+                    creatureFillHolder.spreadX = br.ReadSingle();
+                    creatureFillHolder.spreadY = br.ReadSingle();
+                    creatureFillHolder.spreadZ = br.ReadSingle();
+                    creatureFillHolder.unknown1 = br.ReadInt32();
+                    creatureFillHolder.unknown2 = br.ReadInt32();
+                    creatureFillHolder.unknown3 = br.ReadBoolean();
+                    creatureFillHolder.unknown4 = br.ReadBoolean();
+                    creatureFillHolder.unknown5 = br.ReadBoolean();
+                    creatureFillHolder.unknown6 = br.ReadInt32();
+                    creatureFillHolder.trigger = br.ReadInt32();
+                    creatureFillHolder.unknown7 = br.ReadInt32();
+                    creatureFillHolder.unknown8 = br.ReadInt32();
+                    creatureFillHolder.creatureGroups = new List<CreatureGroup>();
+                    for (x = 0; x < creatureFillHolder.creatureGroupCount; x++)
+                    {
+                        creatureGFillHolder = new CreatureGroup();
+                        creatureGFillHolder.id = br.ReadInt32();
+                        creatureGFillHolder.amount = br.ReadInt32();
+                        creatureGFillHolder.respawn = br.ReadInt32();
+                        creatureGFillHolder.diedTimes = br.ReadInt32();
+                        creatureGFillHolder.agressive = br.ReadInt32();
+                        creatureGFillHolder.offsetWater = br.ReadInt32();
+                        creatureGFillHolder.offsetTrn = br.ReadInt32();
+                        creatureGFillHolder.faction = br.ReadInt32();
+                        creatureGFillHolder.facHelper = br.ReadInt32();
+                        creatureGFillHolder.facAccept = br.ReadInt32();
+                        creatureGFillHolder.needHelp = br.ReadBoolean();
+                        creatureGFillHolder.defFaction = br.ReadBoolean();
+                        creatureGFillHolder.defFacHelper = br.ReadBoolean();
+                        creatureGFillHolder.defFacAccept = br.ReadBoolean();
+                        creatureGFillHolder.pathID = br.ReadInt32();
+                        creatureGFillHolder.loopType = br.ReadInt32();
+                        creatureGFillHolder.speedFlag = br.ReadInt32();
+                        creatureGFillHolder.deadTime = br.ReadInt32();
+                        creatureFillHolder.creatureGroups.Add(creatureGFillHolder);
+                    }
+                    creatureSets.Add(creatureFillHolder);
                 }
-                creatureSets.Add(creatureFillHolder);
+                ResourceSet resourceFillHolder;
+                ResourceGroup resourceGFillHolder;
+                for (i = 0; i < resourceSetsCount; i++)
+                {
+                    resourceFillHolder = new ResourceSet();
+                    resourceFillHolder.spawnX = br.ReadSingle();
+                    resourceFillHolder.spawnY = br.ReadSingle();
+                    resourceFillHolder.spawnZ = br.ReadSingle();
+                    resourceFillHolder.spreadX = br.ReadSingle();
+                    resourceFillHolder.spreadZ = br.ReadSingle();
+                    resourceFillHolder.resourceGroupCount = br.ReadInt32();
+                    resourceFillHolder.unknown1 = br.ReadBoolean();
+                    resourceFillHolder.unknown2 = br.ReadBoolean();
+                    resourceFillHolder.unknown3 = br.ReadBoolean();
+                    resourceFillHolder.unknown4 = br.ReadInt32();
+                    resourceFillHolder.unknown5a = br.ReadByte();
+                    resourceFillHolder.unknown5b = br.ReadByte();
+                    resourceFillHolder.unknown5c = br.ReadByte();
+                    resourceFillHolder.unknownTrigger = br.ReadInt32();
+                    resourceFillHolder.unknown6 = br.ReadByte();
+                    resourceFillHolder.unknown7 = br.ReadBoolean();
+                    resourceFillHolder.unknown8 = br.ReadBoolean();
+                    resourceFillHolder.unknown9 = br.ReadBoolean();
+                    resourceFillHolder.resourceGroups = new List<ResourceGroup>();
+                    for (x = 0; x < resourceFillHolder.resourceGroupCount; x++)
+                    {
+                        resourceGFillHolder = new ResourceGroup();
+                        resourceGFillHolder.type = br.ReadInt32();
+                        resourceGFillHolder.id = br.ReadInt32();
+                        resourceGFillHolder.respawn = br.ReadInt32();
+                        resourceGFillHolder.amount = br.ReadInt32();
+                        resourceGFillHolder.unknown1 = br.ReadInt32();
+                        resourceFillHolder.resourceGroups.Add(resourceGFillHolder);
+                    }
+                    resourceSets.Add(resourceFillHolder);
+                }
+                DynamicObj dynamicObjFillHolder;
+                for (i = 0; i < dynamicsCount; i++)
+                {
+                    dynamicObjFillHolder = new DynamicObj();
+                    dynamicObjFillHolder.id = br.ReadInt32();
+                    dynamicObjFillHolder.spawnX = br.ReadSingle();
+                    dynamicObjFillHolder.spawnY = br.ReadSingle();
+                    dynamicObjFillHolder.spawnZ = br.ReadSingle();
+                    dynamicObjFillHolder.dir1 = br.ReadByte();
+                    dynamicObjFillHolder.dir2 = br.ReadByte();
+                    dynamicObjFillHolder.rad = br.ReadByte();
+                    dynamicObjFillHolder.triggerID = br.ReadInt32();
+                    dynamicObjFillHolder.scale = br.ReadByte();
+                    dynamics.Add(dynamicObjFillHolder);
+                }
+                Trigger triggerFillHolder;
+                for (i = 0; i < triggersCount; i++)
+                {
+                    triggerFillHolder = new Trigger();
+                    triggerFillHolder.id = br.ReadInt32();
+                    triggerFillHolder.gmID = br.ReadInt32();
+                    //name is ALWAYS 128 bytes long
+                    triggerFillHolder.name = br.ReadBytes(128);
+                    triggerFillHolder.autostart = br.ReadBoolean();
+                    triggerFillHolder.autostartDelay = br.ReadInt32();
+                    triggerFillHolder.autostopDelay = br.ReadInt32();
+                    triggerFillHolder.dontStartOnSch = br.ReadBoolean();
+                    triggerFillHolder.dontStoponSch = br.ReadBoolean();
+                    triggerFillHolder.year1 = br.ReadInt32();
+                    triggerFillHolder.month1 = br.ReadInt32();
+                    triggerFillHolder.weekDay1 = br.ReadInt32();
+                    triggerFillHolder.day1 = br.ReadInt32();
+                    //no idea why they didn't use byte here... 
+                    //not like there are 256 hours in a day...
+                    //and for month/weekday/day
+                    triggerFillHolder.hour1 = br.ReadInt32();
+                    //same for this
+                    triggerFillHolder.minute1 = br.ReadInt32();
+                    triggerFillHolder.year2 = br.ReadInt32();
+                    triggerFillHolder.month2 = br.ReadInt32();
+                    triggerFillHolder.weekDay2 = br.ReadInt32();
+                    triggerFillHolder.day2 = br.ReadInt32();
+                    triggerFillHolder.hour2 = br.ReadInt32();
+                    triggerFillHolder.minute2 = br.ReadInt32();
+                    triggerFillHolder.duration = br.ReadInt32();
+                    triggers.Add(triggerFillHolder);
+                }
+                    return true;
             }
-            return true;
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+        /// <summary>
+        /// Load an NPCGEN object from an XML file
+        /// </summary>
+        /// <param name="XMLNPCGenData">Path to XML file containing NPCGen</param>
+        /// <returns>New NPCGEN object</returns>
+        public NPCGEN LoadFromXml(String XMLNPCGenData)
+        {
+            StreamReader sr = new StreamReader(@XMLNPCGenData);
+            return LoadFromXmlString(sr.ReadToEnd());
+        }
+        /// <summary>
+        /// Load an NPCGEN object from an XML string
+        /// </summary>
+        /// <param name="XMLNPCGenData">XML String containing NPCGen</param>
+        /// <returns>New NPCGEN object</returns>
+        public NPCGEN LoadFromXmlString(String XMLNPCGenData)
+        {
+            try
+            {
+                XmlSerializer xs = new XmlSerializer(typeof(NPCGEN));
+                MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(XMLNPCGenData));
+                return xs.Deserialize(ms) as NPCGEN;
+            }
+            catch (Exception e)
+            {
+                Debug.Write(e.Message);
+                return new NPCGEN();
+            }
         }
         /// <summary>
         /// Save an npcgen.data file
         /// </summary>
         /// <param name="NPCGenFile">Path to save npcgen.data to</param>
         /// <returns>True on successful save</returns>
-        public Boolean save(String NPCGenFile)
+        public Boolean Save(String NPCGenFile)
         {
             return true;
+        }
+        /// <summary>
+        /// Serialize npcgen.data to XML
+        /// </summary>
+        /// <param name="fileName">Filename to save to</param>
+        public void ToXml(String fileName)
+        {
+            //XmlSerializer serialize = new XmlSerializer(typeof(NPCGEN));
+            TextWriter tw = new StreamWriter(@fileName);
+            tw.Write(ToXml());
+            //serialize.Serialize(tw, this);
+            tw.Close();
+        }
+        /// <summary>
+        /// Serialize npcgen.data to XML
+        /// </summary>
+        /// <returns>XML Formatted String of npcgen.data</returns>
+        public String ToXml()
+        {
+            XmlSerializer serialize = new XmlSerializer(typeof(NPCGEN));
+            MemoryStream ms = new MemoryStream();
+            serialize.Serialize(ms, this);
+            return Encoding.UTF8.GetString(ms.ToArray());
         }
     }
 }
