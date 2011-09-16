693d7a032e81e916d2a65720529b00b819b01e9f
das7002@gmail.com <das7002@gmail.com@a5ed4baf-b871-94ba-85ba-8b09a3ea3645>
2011-09-16 18:35:15 +0000

    added a NameHandler to ElementsDataClientV136.cs (I'll probably end up moving it into it's own file after I finish with V136)

--- trunk/PW Edit/PWEditLib/cs structs/element/ElementsDataClientV136.cs
+++ trunk/PW Edit/PWEditLib/cs structs/element/ElementsDataClientV136.cs
@@ -4,13 +4,59 @@ using System.Runtime.InteropServices;
 
 namespace PWEditLib.ElementsData.Client.V136
 {
+    internal class NameHandler
+    {
+        internal Byte[] fixedName;
+        internal String sName;
+        public NameHandler(Encoding encoding, String fixFrom, Int32 maxLen)
+        {
+            fixedName = new Byte[maxLen];
+            if (fixFrom != null && !fixFrom.Equals(String.Empty))
+            {
+                Byte[] temp = encoding.GetBytes(fixFrom);
+                if (temp.Length > maxLen)
+                {
+                    for (Int32 i = 0; i < maxLen; i++)
+                    {
+                        fixedName[i] = temp[i];
+                    }
+                }
+                else
+                {
+                    temp.CopyTo(fixedName, 0);
+                }
+            }
+        }
+        public NameHandler(Encoding encoding, Byte[] name)
+        {
+            if (name == null || name[0] == 0)
+            {
+                sName = "Undefined";
+            }
+            else
+            {
+                sName = encoding.GetString(name).TrimEnd('\0');
+            }
+        }
+    }
     /// <summary>
     /// List 1
     /// </summary>
     public class EquipmentAddon
     {
         public UInt32 id;
-        public String name;
+        Byte[] _name;
+        public String name
+        {
+            get
+            {
+                return new NameHandler(Encoding.Unicode, _name).sName;
+            }
+            set
+            {
+                _name = new NameHandler(Encoding.Unicode, value, 128).fixedName;
+            }
+        }
         public Int32 num_params;
         public Int32 param1;
         public Int32 param2;
@@ -33,5 +79,6 @@ namespace PWEditLib.ElementsData.Client.V136
         public String name;
         public String hitgfx;
         public String hitsfx;
+
     }
 }
